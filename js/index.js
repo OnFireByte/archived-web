75172ca50c868459847069c327b94892555cf190
Pakin Puttanukulchai <pakinza1199@hotamail.com>
2021-02-09 17:18:04 +0700

    modernized file ( minified , easy-for-maintainance )
    >add minified js and css
    >remove inline-style and deprecated  tags

--- js/index.js
+++ js/index.js
@@ -7,6 +7,7 @@ function onLoad() {
     "randomAccent_Hello_" + Math.randomRangeInt(1, 4)
   ).style.cssText =
     "background-color:#27e8a7; color:#1d212f;padding:0 2px;margin: 0;font-weight:550;";
+
   document.getElementById(
     "randomAccent_World_" + Math.randomRangeInt(1, 4)
   ).style.cssText =
