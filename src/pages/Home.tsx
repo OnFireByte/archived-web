59991441c97c6885bfedb02ecc87481b4620ebe8
Pakin Puttanukulchai <65996113+OnFireByte@users.noreply.github.com>
2022-06-25 00:35:06 +0700

    remove weird whitespace

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,6 @@ import Block from "@/components/Block";
 const Home = () => {
     return (
         <Block className="w-full">
-            {" "}
             Dunno what im gonna put in homepage, but another tabs is working doe
         </Block>
     );
