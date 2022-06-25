0036eedb0576f89edab55872ee5041dc5dc9c488
Pakin Puttanukulchai <65996113+OnFireByte@users.noreply.github.com>
2022-06-25 14:14:25 +0700

    new Homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,34 @@ import Block from "@/components/Block";
 
 const Home = () => {
     return (
-        <Block className="w-full">
-            Dunno what im gonna put in homepage, but another tabs is working doe
+        <Block
+            title="Home"
+            className="w-full md:h-[45rem] border-0 bg-gradient-to-r from-purple-400 to-pink-600 p-[0.1rem]"
+            titleClassName="ml-5"
+        >
+            <div className=" bg-slate-800 h-full p-5 rounded-lg">
+                <pre className="leading-[0.76rem] md:leading-[1.03rem] text-[0.75rem] md:text-base font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+                    {`
+██████╗░██╗░░░██╗████████╗███████╗  ██████╗░░░░░█████╗░
+██╔══██╗╚██╗░██╔╝╚══██╔══╝██╔════╝  ╚════██╗░░░██╔══██╗
+██████╦╝░╚████╔╝░░░░██║░░░█████╗░░  ░░███╔═╝░░░██║░░██║
+██╔══██╗░░╚██╔╝░░░░░██║░░░██╔══╝░░  ██╔══╝░░░░░██║░░██║
+██████╦╝░░░██║░░░░░░██║░░░███████╗  ███████╗██╗╚█████╔╝
+╚═════╝░░░░╚═╝░░░░░░╚═╝░░░╚══════╝  ╚══════╝╚═╝░╚════╝░`}
+                </pre>
+                <pre className="mt-3">
+                    Welcome to my homepage! <br />
+                    <br />
+                    TLDR: <br />
+                    I'm Pakin Puttanukulchai (Byte), a college student that interested in
+                    programming / web development. <br />
+                    I'm (quite) proficient at Javascript, Python, Tailwindcss. And good at Go,
+                    Typescript, React.
+                    <br />
+                    <br />
+                    You can see my info in About page.
+                </pre>
+            </div>
         </Block>
     );
 };
